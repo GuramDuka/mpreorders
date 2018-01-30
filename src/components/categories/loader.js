bbb5a5a12e2456a677814e7ba685054576f9882c
Guram Duka <guram.duka@gmail.com>
2018-01-30 13:35:39 +0300

    test

--- src/components/categories/loader.js
+++ src/components/categories/loader.js
@@ -1,6 +1,7 @@
 //------------------------------------------------------------------------------
 import { bfetch } from '../../backend/backend';
 import disp from '../../lib/store';
+import { headerTitleStorePath } from '../../const';
 import { successor, failer, starter } from '../load';
 //------------------------------------------------------------------------------
 export const storePrefix = 'category';
@@ -9,7 +10,8 @@ export default function loader(success, fail, start) {
 	return bfetch(
 		{ r: { m: 'category', f: 'list', r: { target: 'products' } } },
 		successor(
-			result => disp(store => store.setIn(storePrefix + '.list', result)),
+			result => disp(store => store.setIn(storePrefix + '.list', result)
+				.setIn(headerTitleStorePath, 'Категории')),
 			success
 		),
 		failer(fail),
